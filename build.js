63889e6c116ae9d4e8437b16e1856b992def9d36
Ronan LE MEILLAT <ronan.le_meillat@sctg.eu.org>
2022-01-28 16:40:18 +0100

    initial

---
+++ build.js
@@ -0,0 +1,35 @@
+var fs = require('fs')
+var UglifyJS = require('uglify-js');
+
+function md(dir) {
+  fs.mkdirSync(dir, { recursive: true }, (err) => {
+    if (err) throw err;
+  });
+}
+
+function minify(fin, fout) {
+  fs.readFile(fin, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    var result = UglifyJS.minify(data);
+    fs.writeFile(fout, result.code, function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("File " + fout + " was successfully saved.");
+      }
+    });
+  });
+}
+
+function callbackErr(err) {
+  if (err) throw err;
+  console.log('File copied');
+}
+
+md('./build');
+md('./build/js');
+md('./build/css');
+fs.copyFile('./public/index.html','./build/index.html',callbackErr);
